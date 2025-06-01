cd1911d8edb64fdf2e56c1c4fc66d437a58b1ed3
Saurabh Kumar <saurabh01kumar0041@gmail.com>
2025-06-01 17:08:29 +0530

    first commit

---
+++ store/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Product
+
+# Register your models here.
+
+# to prepopulate the slug we need to do this
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug' : ('product_name',)
+    }
+
+    list_display = ('product_name', 'price', 'category', 'modified_date')
+
+admin.site.register(Product, ProductAdmin)
